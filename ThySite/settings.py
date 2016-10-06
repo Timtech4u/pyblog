3271668d08ecab0733f8b887a8ad7d7d24f8cd24
Timtech4u <timothyolaleke@gmail.com>
2016-10-06 09:27:02 +0000

    Update settings.py

--- ThySite/settings.py
+++ ThySite/settings.py
@@ -119,5 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, "static"),
+]
+
+#Added Media Files Dir...
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
